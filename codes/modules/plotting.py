36d0cbf9c7728a462d5e6cd94af6dba7ee7d72bf
lauragoms <lauragoms@gmail.com>
2025-11-14 11:44:20 +0100

    :memo:

--- codes/modules/plotting.py
+++ codes/modules/plotting.py
@@ -34,3 +34,19 @@ def map_Delta_W_conductance(DeltaR,WR,G,title='Local gap map '):
     plt.colorbar(cbar,label='G')
 
     plt.show()
+
+
+def map_Delta_W_localgap(DeltaR,WR,lg,title='Local gap map '):
+    plt.style.use('science')
+
+    plt.figure(figsize=(8,6))
+
+    cbar = plt.pcolormesh(DeltaR,WR,lg,cmap=cm.cividis,shading='gouraud')
+
+
+    plt.title(title,fontsize=20)
+    plt.xlabel('$\Delta$',fontsize=20)
+    plt.ylabel('W',fontsize=20)
+    plt.colorbar(cbar,label='Local Gap')
+
+    plt.show()
