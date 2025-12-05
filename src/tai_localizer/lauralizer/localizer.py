54719710498472c5085ca9696e83ff38a19275d5
lauragoms <lauragoms@gmail.com>
2025-12-05 18:06:14 +0100

    la vida nos sonrie, clean localizer implemented

---
+++ src/tai_localizer/lauralizer/localizer.py
@@ -0,0 +1,73 @@
+
+
+import numpy as np
+import scipy.sparse as sp
+import kwant
+
+from tai_localizer.perulizer.misc import sigma_x, sigma_y, sigma_z, sigma_0
+from .functions import _fast_pfaffian, eigsh, find_dos_gap
+
+
+def spectral_localizer_AII2D(
+    positions: list,
+    ham: sp.csr_matrix,
+    E0: float,
+    kappa: float,
+    rotated: bool = True,
+) -> sp.csr_matrix:
+    """
+    Spectral Localizer for class AII2D
+    
+    :param positions: Position of the sites
+    :type positions: list
+    :param ham: Sparse Hamiltonian of the system
+    :type ham: sp.csr_matrix
+    :param E0: Energy to compute the spectral localizer
+    :type E0: float
+    :param kappa: Parameter Kappa for the spectral localizer
+    :type kappa: float
+    :param rotated: True for computing the pfaffian
+    :type rotated: bool
+    """
+    norbs = 4
+    Ls = ham.shape[0] // norbs
+    x0, y0 = np.average(positions, axis=0)
+
+    X = sp.diags(np.kron(positions[:, 0], [1] * 4))
+    Y = sp.diags(np.kron(positions[:, 1], [1] * 4))
+
+    TR = sp.kron(sp.kron(sp.eye(Ls), sigma_0), sigma_y)
+    D = (X - (x0) * sp.eye(norbs * Ls)) + 1j * (Y - (y0) * sp.eye(norbs * Ls))
+    Q = (1 / np.sqrt(2)) * sp.bmat(
+        [[sp.eye(norbs * Ls), TR], [-TR, sp.eye(norbs * Ls)]]
+    )
+    h = ham - E0 * sp.eye(norbs * Ls)
+
+    localizer = sp.bmat([[h, kappa * np.conjugate(D)], [kappa * D, -h]])
+    loc_rotated = 1j * np.conjugate(Q) @ localizer @ Q
+
+    return loc_rotated if rotated else localizer
+
+def invariant_localizer(loc_rotated: np.array):
+    invariant = np.sign(_fast_pfaffian(loc_rotated)[0])
+    return invariant
+
+
+def local_gap_localizer(localizer: sp.csr_matrix):
+        local_gap = eigsh(localizer, k=1, sigma=0)
+        return np.abs(local_gap)
+
+
+def dos_kpm(L: sp.csr_matrix,
+            bounds: tuple = (-1, 1),
+            number_points: int = 200,
+            energy_resol: float = 0.01):
+    
+    es = np.linspace(*bounds, number_points)
+    spectrum = kwant.kpm.SpectralDensity(hamiltonian=L)
+    spectrum.add_moments(energy_resolution=energy_resol)
+    density_subset = spectrum(es)
+    DOS = np.real(density_subset) / np.max(np.real(density_subset))
+
+    gap = find_dos_gap(es, DOS)
+    return DOS, gap
--- src/tai_localizer/perulizer/misc.py
+++ src/tai_localizer/perulizer/misc.py
@@ -3,4 +3,6 @@ import numpy as np
 sigma_x = np.array([[0, 1], [1, 0]])
 sigma_y = np.array([[0, -1j], [1j, 0]])
 sigma_z = np.array([[1, 0], [0, -1]])
+sigma_0 = np.array([[1, 0], [0, 1]])
+
 bhz_trs_operator = np.kron(np.eye(2), np.array([[0, 1], [-1, 0]]))
