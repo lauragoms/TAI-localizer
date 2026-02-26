731a6deae5141c8475162778a316a21b23cde7eb
lauragoms <lauragoms@gmail.com>
2026-02-26 13:00:53 +0100

    check points with one node

--- submit.sh
+++ submit.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 #SBATCH --job-name=adaptive_disorder
+#SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
+##SBATCH --mem=100G
 #SBATCH --time=5:00:00
 #SBATCH --partition=short
 #SBATCH --output=adaptive_%j.out
@@ -11,4 +13,4 @@ module load MPI/GCC/8.3.1/openmpi-4.2.1
 source /home/lgomez/miniconda3/etc/profile.d/conda.sh
 conda activate peru_env
 
-srun --mpi=pmi2 python adaptive_fig3D_cluster.py
+mpirun -np $SLURM_NTASKS python -m mpi4py.futures adaptive_fig3D_cluster.py
