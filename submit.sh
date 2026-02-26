233a4ca871d4a7263a1511a98d56573b1e0192a6
lauragoms <lauragoms@gmail.com>
2026-02-26 11:19:18 +0100

    checknpoints

--- submit.sh
+++ submit.sh
@@ -2,16 +2,13 @@
 #SBATCH --job-name=adaptive_disorder
 #SBATCH --ntasks=10
 #SBATCH --time=5:00:00
-#SBATCH --partition=general
-#SBATCH --qos=regular
+#SBATCH --partition=short
 #SBATCH --output=adaptive_%j.out
 #SBATCH --error=adaptive_%j.err
 
 module purge
-module load OpenMPI/5.0.7-GCC-14.2.0
-module load Miniforge3  
-source $(conda info --base)/etc/profile.d/conda.sh
-conda activate /home/grushin/.conda/envs/peru_env
-### mpiexec -n 10 /home/grushin/.conda/envs/peru_env/bin/python -m mpi4py.futures adaptive_fig3D_cluster.py
-### srun -n $SLURM_NTASKS --mpi=pmi2 /home/grushin/.conda/envs/peru_env/bin/python -m mpi4py.futures adaptive_fig3D_cluster.py
-mpiexec -n 10 /home/grushin/.conda/envs/peru_env/bin/python -m mpi4py.futures adaptive_fig3D_cluster.py
+
+source /home/lgomez/miniconda3/etc/profile.d/conda.sh
+conda activate peru_env
+
+srun python adaptive_fig3D_cluster.py
