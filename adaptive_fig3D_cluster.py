b4db0e652febf2664a4cd9e5d94d897224cbd711
lauragoms <lauragoms@gmail.com>
2026-02-27 10:20:21 +0100

    ready to run

--- adaptive_fig3D_cluster.py
+++ adaptive_fig3D_cluster.py
@@ -2,10 +2,7 @@ import adaptive
 from func_for_fig3D import params_obs_3D
 from mpi4py.futures import MPIPoolExecutor
 from mpi4py import MPI
-import scipy.sparse as sp
-import scipy.sparse.linalg as sla
-from scipy.sparse import identity
-import mumps
+
 
 comm = MPI.COMM_WORLD
 print(comm.Get_rank(), comm.Get_size())
@@ -18,66 +15,34 @@ num_realizations = 100
 def goal(ps):
     return ps.npoints > 5000
 
-# dummy function for testing mumps in adaptive:
-
-
-def sparse_diag(matrix, k, sigma, **kwargs):
-    """Call sla.eigsh with mumps support.
-
-    See scipy.sparse.linalg.eigsh for documentation.
-    """
-    class LuInv(sla.LinearOperator):
-        def __init__(self, A):
-            inst = mumps.Context(comm=MPI.COMM_SELF)
-            inst.analyze(A, ordering='pord')
-            inst.factor(A)
-            self.solve = inst.solve
-            sla.LinearOperator.__init__(self, A.dtype, A.shape)
-
-        def _matvec(self, x):
-            return self.solve(x.astype(self.dtype))
-
-    opinv = LuInv(matrix - sigma * identity(matrix.shape[0]))
-    return sla.eigsh(matrix, k, sigma=sigma, OPinv=opinv, **kwargs)
-
-
-def mumps_test(A, B):
-    matrix = sp.random(100, 100, density=0.01, format='csr') * A
-    matrix += sp.eye(100) * B
-    evals, evecs = sparse_diag(matrix, k=10, sigma=0)
-    return evals[0] 
-
 
 def f(dis_MJ):
-    return mumps_test(A=dis_MJ[0], B=dis_MJ[1])
+    # lattice params
+    system_size = 10
 
-# def f(dis_MJ):
-#     # lattice params
-#     system_size = 10
+    # sys params
+    A = 1
+    num_realizations = 1
+    bond_lengthscale = 1 / system_size
+    bond_power = 1 / system_size
 
-#     # sys params
-#     A = 1
-#     num_realizations = 100
-#     bond_lengthscale = 1 / system_size
-#     bond_power = 1 / system_size
-
-#     # localizer params
-#     kappa = 2
-#     E0 = 0
-#     return params_obs_3D(
-#         system_size=system_size,
-#         MJ=dis_MJ[0],
-#         A=A,
-#         onsite_disorder=dis_MJ[1],
-#         disorder_average=num_realizations,
-#         bond_lengthscale=bond_lengthscale,
-#         bond_power=bond_power,
-#         kappa_spec=kappa,
-#         E0=E0,
-#         sigma=0.,
-#         kappa_shift=0,
-#         beta=1,
-#     )
+    # localizer params
+    kappa = 2
+    E0 = 0
+    return params_obs_3D(
+        system_size=system_size,
+        MJ=dis_MJ[0],
+        A=A,
+        onsite_disorder=dis_MJ[1],
+        disorder_average=num_realizations,
+        bond_lengthscale=bond_lengthscale,
+        bond_power=bond_power,
+        kappa_spec=kappa,
+        E0=E0,
+        sigma=0.,
+        kappa_shift=0,
+        beta=1,
+    )
 
 
 if __name__ == "__main__":
