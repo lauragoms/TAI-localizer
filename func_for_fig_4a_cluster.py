dd7731b734297bc7cb2386af7cf0b316bb2e80b1
lauragoms <lauragoms@gmail.com>
2026-03-04 12:23:39 +0100

    maxworkeres

--- func_for_fig_4a_cluster.py
+++ func_for_fig_4a_cluster.py
@@ -63,7 +63,7 @@ if __name__ == "__main__":
 
     runner_dis = adaptive.Runner(
         learner_dis,
-        executor=MPIPoolExecutor(),
+        executor=MPIPoolExecutor(max_workers=comm.Get_size()),
         shutdown_executor=True,
         goal=goal,
     )
