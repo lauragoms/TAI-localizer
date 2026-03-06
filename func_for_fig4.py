f8e6f95f9283591a8fa6f59728cbc2e84f66252c
lauragoms <lauragoms@gmail.com>
2026-03-06 10:54:27 +0100

    finalyfigure 4a,4b,4c

--- func_for_fig4.py
+++ func_for_fig4.py
@@ -1,4 +1,3 @@
-import kwant
 import scipy.sparse as sp
 import numpy as np
 from tai_localiser.lauralizer.amorphous_model_3D import (
@@ -10,30 +9,7 @@ from tai_localiser.lauralizer.localizer import (
     sign_det
     )
 from koala import pointsets
-import pickle as pi
-
-def grid_3D(nx: int, ny: int, nz: int) -> np.ndarray:
-    """Generates a uniformly spaced grid of points in 3D
-
-    Args:
-        nx (int): Number of points in x direction
-        ny (int): Number of points in y direction
-        nz (int): Number of points in z direction
-
-    Returns:
-        np.ndarray: List of all the positions.
-    """
-
-    pos_x = np.linspace(0, 1, nx, endpoint=False)
-    pos_y = np.linspace(0, 1, ny, endpoint=False)
-    pos_z = np.linspace(0, 1, nz, endpoint=False)
-    pos_x += 0.5*pos_x[0]
-    pos_y += 0.5*pos_y[0]
-    pos_z += 0.5*pos_z[0]
-    g_out = np.reshape(np.meshgrid(pos_x, pos_y, pos_z), [3, -1]).T
-    return g_out
-
-# sites = grid_3D(system_size, system_size, system_size)
+from koala.pointsets import grid
 
 
 def params_obs_3D(
@@ -54,14 +30,16 @@ def params_obs_3D(
     **kwargs,
 ):
     # create lattice:
-    sites = grid_3D(system_size, system_size, system_size)
+    sites = grid(system_size, system_size, system_size)
 
     idx_dis = []
 
     for seed in range(disorder_average):
         rng = np.random.default_rng(seed)
         # structural disorder
-        sites = pointsets.move_all_points(sites, sigma, kappa_shift, beta, resolution=resolution)
+        sites = pointsets.move_all_points(
+            sites, sigma, kappa_shift, beta, resolution=resolution
+            )
 
         # create bonds
         bond_distance = 1.3 / system_size
