bc2e078cf013543d62cafd8bc038599e1743dd24
lauragoms <lauragoms@gmail.com>
2026-04-17 10:46:10 +0200

    removed things in lauralizer

--- src/tai_localiser/lauralizer/amorphous_model_BHZ_2D.py
+++ src/tai_localiser/lauralizer/amorphous_model_BHZ_2D.py
@@ -74,63 +74,6 @@ def amorph_hopping(
     return hopping_multiplier*(hop_strength + hop_x_SOC + hop_y_SOC)
 
 
-"""amorphous system"""
-
-
-def Displacement_2D(sites: list, seed: int, sigma: float):
-    """Given a crystalline system, the sites are displaced by a Gaussian
-    distribution with standard deviation sigma
-
-    Parameters
-    -----------------
-    sites : fsyst.sites
-        List of sites from kwant builder
-    sigma : float
-        Standar deviation for the Gaussian displacement of sites
-
-    Returns
-    -----------------
-    amorph_a,amorph_b,Sites
-    """
-
-    disp_sites = []
-    np.random.seed(int(seed))
-    print("seed σ:", seed)
-    disp = np.random.normal(0, sigma, size=2 * len(sites))
-
-    print("sigma: ", sigma)
-    for i in range(len(sites)):
-
-        x, y = sites[i][0], sites[i][1]
-
-        disp_x, disp_y = disp[2*i:2*i+2]
-        x = x + disp_x
-        y = y + disp_y
-        disp_sites.append(np.array([x, y]))
-
-    return disp_sites
-
-
-def sites_bonds_generator(
-    Lx: int, Ly: int, dis_amorph: float, seed_amorph: int, koala=True
-):
-
-    sites = [(x, y) for x in range(-Lx, Lx) for y in range(-Ly, Ly)]
-
-    if koala is False:
-        sites = Displacement_2D(sites, sigma=dis_amorph, seed=seed_amorph)
-    else:
-        sites = koala
-
-    bonds = bonds_func(sites)
-
-    return sites, bonds
-
-
-# rng_W = np.random.default_rng(int(seed_onsite))
-# rng_hdmd = np.random.default_rng(int(seed_hadamard))
-
-
 def amorph_BHZ(sites, bonds):
     """
     Returns an amorphous 2D BHZ model system with given parameters.
